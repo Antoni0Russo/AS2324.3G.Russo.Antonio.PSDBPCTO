5404a9176cfeccba3e7d80c9f4654397be14b5ed
Antonio Russo <Utente@LAPTOP-5C0S9O65>
2024-05-10 09:46:52 +0200

    feat : correzione di errori e inizalizzazione del secondo punto

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,8 @@
             dgvDati = new DataGridView();
             btnPiloti = new Button();
             btnGare = new Button();
+            cmbSeleziona = new ComboBox();
+            btnSeleziona = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvDati).BeginInit();
             SuspendLayout();
             // 
@@ -46,7 +48,7 @@
             // 
             // btnPiloti
             // 
-            btnPiloti.Location = new Point(490, 59);
+            btnPiloti.Location = new Point(564, 28);
             btnPiloti.Name = "btnPiloti";
             btnPiloti.Size = new Size(113, 45);
             btnPiloti.TabIndex = 1;
@@ -56,7 +58,7 @@
             // 
             // btnGare
             // 
-            btnGare.Location = new Point(168, 59);
+            btnGare.Location = new Point(116, 28);
             btnGare.Name = "btnGare";
             btnGare.Size = new Size(113, 45);
             btnGare.TabIndex = 1;
@@ -64,11 +66,32 @@
             btnGare.UseVisualStyleBackColor = true;
             btnGare.Click += btnGare_Click;
             // 
+            // cmbSeleziona
+            // 
+            cmbSeleziona.FormattingEnabled = true;
+            cmbSeleziona.Items.AddRange(new object[] { "Elenco squadre non italiane ordinate per nome", "Elenco piloti Ducati ordinati per cognome e nome", "Elenco circuiti ordinati per nome" });
+            cmbSeleziona.Location = new Point(64, 120);
+            cmbSeleziona.Name = "cmbSeleziona";
+            cmbSeleziona.Size = new Size(374, 28);
+            cmbSeleziona.TabIndex = 2;
+            // 
+            // btnSeleziona
+            // 
+            btnSeleziona.Location = new Point(473, 120);
+            btnSeleziona.Name = "btnSeleziona";
+            btnSeleziona.Size = new Size(96, 30);
+            btnSeleziona.TabIndex = 3;
+            btnSeleziona.Text = "Seleziona";
+            btnSeleziona.UseVisualStyleBackColor = true;
+            btnSeleziona.Click += btnSeleziona_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnSeleziona);
+            Controls.Add(cmbSeleziona);
             Controls.Add(btnGare);
             Controls.Add(btnPiloti);
             Controls.Add(dgvDati);
@@ -83,5 +106,7 @@
         private DataGridView dgvDati;
         private Button btnPiloti;
         private Button btnGare;
+        private ComboBox cmbSeleziona;
+        private Button btnSeleziona;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -79,5 +79,78 @@ namespace AS2324._3G.Russo.Antonio.PSDBPCTO
                 MessageBox.Show(exe.Message);
             }
         }
+
+        private void btnSeleziona_Click(object sender, EventArgs e)
+        {
+            // contenitore per i dati estratti
+            DataTable dtDati = new DataTable();
+            switch (cmbSeleziona.SelectedItem.ToString())
+            {
+
+                case "Elenco squadre non italiane ordinate per nome":
+                    string query = "";
+                    string strConnessione = "";
+
+                    // https://www.connectionstrings.com/
+
+                    // imposta la connessione
+
+                    // sqlite Data Source=c:\mydb.db;Version=3;
+                    strConnessione = "Data Source=" + @"C:\Users\Utente\Desktop\flow\AS2324.3G.Russo.Antonio.PSDBPCTO\MotoGP.db" + ";Version=3;";
+
+                    
+
+                    // query per l'estrazione dei dati dal database
+                    query = "SELECT * FROM Squadre WHERE nazionalita <> 'ITA' ORDER BY nome;";
+
+                    //connessione e query
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(query, strConnessione);
+
+                    try
+                    {
+                        // riempie il DataTable
+                        da.Fill(dtDati);
+
+                        // binda la DataGridView
+                        dgvDati.DataSource = dtDati;
+                    }
+                    catch (Exception exe)
+                    {
+                        MessageBox.Show(exe.Message);
+                    }
+                    break;
+                case "Elenco piloti Ducati ordinati per cognome e nome":
+                   
+
+                    // https://www.connectionstrings.com/
+
+                    // imposta la connessione
+
+                    // sqlite Data Source=c:\mydb.db;Version=3;
+                    strConnessione = "Data Source=" + @"C:\Users\Utente\Desktop\flow\AS2324.3G.Russo.Antonio.PSDBPCTO\MotoGP.db" + ";Version=3;";
+
+                   
+
+                    // query per l'estrazione dei dati dal database
+                    query = "SELECT * FROM Squadre WHERE nazionalita <> 'ITA' ORDER BY nome;";
+
+                    //connessione e query
+                    SQLiteDataAdapter dai = new SQLiteDataAdapter(query, strConnessione);
+
+                    try
+                    {
+                        // riempie il DataTable
+                        dai.Fill(dtDati);
+
+                        // binda la DataGridView
+                        dgvDati.DataSource = dtDati;
+                    }
+                    catch (Exception exe)
+                    {
+                        MessageBox.Show(exe.Message);
+                    }
+                    break;
+            }
+        }
     }
 }
